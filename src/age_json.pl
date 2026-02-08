1d85a16949d181c1103de870b7e25123c088de56
Christopher Caldwell <dufflerpud@yahoo.com>
2026-02-08 17:46:18 -0500

    Moved age_json to cpi_cgi as older_json

--- src/age_json.pl
+++ src/age_json.pl
@@ -7,6 +7,7 @@ use lib "/usr/local/lib/perl";
 use cpi_file qw( fatal read_file write_file cleanup );
 use cpi_arguments qw( parse_arguments );
 use cpi_compress_integer qw( compress_integer );
+use cpi_cgi qw( older_json );
 
 our %ARGS;
 our $exit_stat = 0;
@@ -23,42 +24,6 @@ sub usage
     }
 
 #########################################################################
-#	Remove various things that are not legal in old javascript.	#
-#########################################################################
-sub age_json
-    {
-    my( $str ) = @_;
-
-    $str =~ s/\bconst\b/var/gms;	# Old javascript doesn't know "const"
-
-    my @out_strings;
-
-    foreach my $pc ( split(/\b(for\s*\(.*?\)\s*{)/ms,$str) )
-        {
-	if( $pc !~ /for\s*\(\s*var\s+([\w_]+)\s+(of|in)\s+(.*?)\s*\)(\s*)\{/ms )
-	    { push( @out_strings, $pc ); }
-	else
-	    {
-	    my $varname = $1;
-	    my $inof = $2;
-	    my $arrayname = $3;
-	    my $whitespace = $4;
-	    my $instance = &compress_integer( $ctr++ );
-	    push( @out_strings,
-		"var ${arrayname}_keys${instance}=Object.keys($arrayname); for( var ${varname}_ojs${instance}=0; ${varname}_ojs${instance}<${arrayname}_keys${instance}.length; ${varname}_ojs${instance}++ )${whitespace}{ var $varname=",
-		( $inof eq "in"
-		? "${arrayname}_keys${instance}\[${varname}_ojs${instance}\];"
-		: "${arrayname}\[${arrayname}_keys${instance}\[${varname}_ojs${instance}\]\];"
-		) );
-	    }
-	}
-
-    # Yeah, we could just return the array and let write_file put it back
-    # together again.  I just prefer the idea of string-in => string-out.
-    return join("",@out_strings);
-    }
-
-#########################################################################
 #	Main								#
 #########################################################################
 
@@ -72,7 +37,7 @@ sub age_json
 
 &write_file(
     $ARGS{output_file},
-    &age_json(
+    &older_json(
 	&read_file( $ARGS{input_file} ) ) );
 
 &cleanup( $exit_stat );
